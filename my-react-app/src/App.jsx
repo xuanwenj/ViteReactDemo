4ec0ff27326c182ec79833b27cf33b9bb98d2172
xuanwenj <139101662+xuanwenj@users.noreply.github.com>
2024-02-28 10:54:05 +1300

    External CSS style on Button component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,10 +2,12 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Button from './Button.jsx'
 function App() {
   return(
     <>
        <Header/>
+       <Button></Button>
        <Food/>
        <Card/>
        <Footer/>
---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,11 @@
+//style react components with css
+// not including external froamworks or preprocessors
+
+//include basic techs: external, modules, inline
+
+function Button(){
+    return(<button className="button">Click me</button>);
+    
+}
+
+export default Button
