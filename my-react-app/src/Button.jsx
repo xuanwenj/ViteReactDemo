5d80dc766d3bb723c983c6928f1576c1fa53e611
xuanwenj <139101662+xuanwenj@users.noreply.github.com>
2024-02-28 20:50:47 +1300

    Inline style on button component

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -2,10 +2,23 @@
 // not including external froamworks or preprocessors
 
 //include basic techs: external, modules, inline
-import styles from './button.module.css'
+
 // modules avoid naming conflicts, because a unique class is generated via a hashing algorithm
+
+
+
+
 function Button(){
-    return(<button className={styles.button}>Click me</button>);
+    //inline style css
+    const styles = { 
+        backgroundColor: "darkseagreen",
+        color:"white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        cursor: "pointer",
+       
+}
+    return(<button style={styles}>Click me</button>);
     
 }
 
