2d4fcd2104f7c47b8d4b2ee0d57fb01c236bf98d
xuanwenj <139101662+xuanwenj@users.noreply.github.com>
2024-03-04 21:53:21 +1300

    Conditions on clicking

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,8 +1,19 @@
 
 function Button(){
-    const handleClick = () => console.log("ouch");
-    const handleClick2 = (name) => console.log(`${name} stop clikcing me`);
-return(<button onClick={() => handleClick2("M")}>Click me 😊</button>);
+    //use {} for codes more than one line in arrow function
+    let count = 0;
+
+    const handleClick = (name) => {
+        if(count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} times`);
+        }else{
+            console.log(`${name} stop clicking me!`)
+        }
+
+    }
+    //const handleClick2 = (name) => console.log(`${name} stop clikcing me`);
+return(<button onClick={() => handleClick("K")}>Click me 😊</button>);
 
 }
 export default Button
