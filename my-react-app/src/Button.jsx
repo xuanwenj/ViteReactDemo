faadd2ab3be2ad23b33fc28860f6a6e870675ab7
xuanwenj <139101662+xuanwenj@users.noreply.github.com>
2024-03-04 22:48:26 +1300

    Event parameter

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -3,17 +3,13 @@ function Button(){
     //use {} for codes more than one line in arrow function
     let count = 0;
 
-    const handleClick = (name) => {
-        if(count < 3){
-            count++;
-            console.log(`${name} you clicked me ${count} times`);
-        }else{
-            console.log(`${name} stop clicking me!`)
-        }
+    //event parameter
 
-    }
-    //const handleClick2 = (name) => console.log(`${name} stop clikcing me`);
-return(<button onClick={() => handleClick("K")}>Click me 😊</button>);
+    const handleClick = (e) => e.target.textContent = "OUCH! 😣";
+
+
+   
+return(<button onDoubleClick={(e) => handleClick(e)}>Click me 😊</button>);
 
 }
 export default Button
